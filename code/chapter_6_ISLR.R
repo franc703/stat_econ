b32079b03058f75d6b004250be89aba9a9ff3954
Rodrigo Franco <franc703@umn.edu>
2022-08-07 00:49:00 -0500

    Finish running all exampls of ridge regression

--- code/chapter_6_ISLR.R
+++ code/chapter_6_ISLR.R
@@ -124,5 +124,39 @@ sqrt(sum(coef(ridge.mod)[-1, 50])^2)
 
 predict(ridge.mod, s = 50, type = "coefficients")[1:20, ]
 
+# split data into train and test
+set.seed(1)
+train <- sample(1:nrow(x), nrow(x)/2)
+test <- (-train)
+y.test <- y[test]
+
+ridge.mod <- glmnet(x[train, ], y[train], alpha = 0, lambda = grid, thresh = 1e-12)
+ridge.pred <- predict(ridge.mod, s = 4, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+
+mean((mean(y[train]) - y.test)^2)
+
+ridge.pred <- predict(ridge.mod, s = 1e10, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+
+# comparison between ridge regression and least squares regression
+ridge.pred <- predict(ridge.mod, s = 0, newx = x[test, ], exact = T, x = x[train, ], y = y[train])
+mean((ridge.pred - y.test)^2)
+least.mod <- lm(y ~ x, subset = train)
+predict(ridge.mod, s = 0, exact = T, type = "coefficients", x = x[train, ], y = y[train])[1:20, ]
+
+set.seed(1)
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 0)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+bestlam
+ridge.pred <- predict(ridge.mod, s = bestlam, newx = x[test, ])
+mean((ridge.pred - y.test)^2)
+
+out <- glmnet(x, y , alpha = 0)
+predict(out, type = "coefficients", s = bestlam)[1:20, ]
+
+# The lasso
+
 
 

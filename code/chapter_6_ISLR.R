7986ccba6835557b38699881cccc14ff94951d5f
Rodrigo Franco <franc703@umn.edu>
2022-08-07 01:00:30 -0500

    Finishing running the lasso examples

--- code/chapter_6_ISLR.R
+++ code/chapter_6_ISLR.R
@@ -157,6 +157,20 @@ out <- glmnet(x, y , alpha = 0)
 predict(out, type = "coefficients", s = bestlam)[1:20, ]
 
 # The lasso
+lasso.mod <- glmnet(x[train, ], y[train], alpha = 1, lambda = grid)
+plot(lasso.mod)
+
+set.seed(1)
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 1)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+lasso.pred <- predict(lasso.mod, s = bestlam, newx = x[test, ])
+mean((lasso.pred - y.test)^2)
+
+out <- glmnet(x, y, alpha = 1, lambda = bestlam)
+lasso.coef <- predict(out, type = "coefficients", s = bestlam)[1:20, ]
+lasso.coef
+lasso.coef[lasso.coef != 0]
 
 
 
